67c8be9a9f1b99b8bd5a3f79df6a98cd6f84a33b
Spud <jeffrypig23@gmail.com>
2022-11-04 22:46:12 -0800

    messing with shellcode

--- shell.asm
+++ shell.asm
@@ -1,16 +1,30 @@
-section .text
+BITS 64
 
-global _start
+;shell db "/bin/sh",0
+;bin db "/bin" ; 2f 62 69 6e
+;sh db "/sh", 0 ; 2f 73 68
 
-_start:
-	mov rax, 59
-	xor rdi, rdi
-	push rdi
-	mov rdi, 0x68732f6e69622f
-	push rdi
-	mov rdi, rsp
-	;mov rsi, 0
-	;xor rsi, rsi
-	mov rdx, 0
-	;xor rdx, rdx
-	syscall
+push 59
+pop rax
+
+;mov rax, 59
+;xor rdi, rdi
+;push rdi
+;mov rdi, 0x68732f6e69622f
+;push 0x68732f6e69622f
+;push 0x69622f00
+push 0x68732f6e
+;push shell
+;push sh
+;push bin
+;push rdi
+;push 0x68732f
+;push 0x6e69622f
+;push 0x0068732f
+;push 0x6e69622f
+mov rdi, [rsp]
+;mov rsi, 0
+xor rsi, rsi
+;mov rdx, 0
+xor rdx, rdx
+syscall
---
+++ shell.c
@@ -0,0 +1,9 @@
+//
+// Created by spud on 11/4/22.
+//
+
+#include <stdlib.h>
+int main() {
+	system("/bin/sh");
+	return 0;
+}
