f1f547fb96e951126cc1a3b2a02f5594060df196
Spud <jeffrypig23@gmail.com>
2022-11-07 20:10:25 -0900

    Made a more advanced build script

--- build.sh
+++ build.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
 cmake -DCMAKE_BUILD_TYPE=Debug .
-cmake --build .
+cmake --build . --target hash
+
+if [ $? -eq 0 ]; then
+  sudo chown root:root hash
+  sudo chmod u+s hash
+
+  if [ $? -eq 0 ]; then
+    rm -r CMakeFiles/
+    rm cmake_install.cmake
+    rm CMakeCache.txt
+  fi
+fi
