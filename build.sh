672858a9f3ccf12c7eda122761ba14c73ee561b5
Spud <jeffrypig23@gmail.com>
2022-11-08 15:53:32 -0900

    decided to add a standalone shellcode file

--- build.sh
+++ build.sh
@@ -10,5 +10,7 @@ if [ $? -eq 0 ]; then
     rm -r CMakeFiles/
     rm cmake_install.cmake
     rm CMakeCache.txt
+    echo -e "\e[1;31m Be sure to leave only the hash program and .shellcode file for the guest user. \e[0m"
+    echo -e "\e[1;32m Good luck, and happy hacking! c: \e[0m"
   fi
 fi
