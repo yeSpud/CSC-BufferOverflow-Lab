e79f3e63c7a3e889ae1f5110ef9cd3979e8652fd
Spud <jeffrypig23@gmail.com>
2022-11-29 14:24:50 -0900

    Revert "Use /bin/bash instead of bin/sh"
    This reverts commit f2e37945fce5c1673090ee3247ad5a862dfbe062.

--- shell.asm
+++ shell.asm
@@ -10,10 +10,9 @@ _start:
 	push 59     ;sys_execve
 	pop rax
 	xor rsi, rsi ;NULL
-    xor rdx, rdx ;NULL
+	xor rdx, rdx ;NULL
 	push rdi
-	;mov rdi, 0x68732F2f6e69622F ;/bin/sh in reverse
-	mov rdi, 0x616268732F2f6e69622F ; /bin/bash in reverse
+	mov rdi, 0x68732F2f6e69622F ;/bin/sh in reverse
 	push rdi
 	mov rdi, rsp ;pointer to the /bin/sh
 	syscall
